a294fbc1699ad190b79a24c16e755b3e0505349d
Betty A. Kim <arkim@selvas.com>
2016-12-27 16:57:14 +0900

    add log

--- apiai-booking-webhook/app.py
+++ apiai-booking-webhook/app.py
@@ -63,15 +63,15 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
     print("send id: ")
     print(recipient_id)
     
     print("access_token: ")
     print(os.environ["PAGE_ACCESS_TOKEN"])
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
     
     
 
