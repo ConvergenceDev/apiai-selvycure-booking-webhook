d37b26b1d84c4d2eb9caa38c49dcabe9486e25b0
arkim <selvasai.convdev@gmail.com>
2016-12-29 14:28:58 +0900

    action 처리 부분 코드 분리

---
+++ action_processor.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import json
+import os
+
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+
+FACEBOOK_SEND_URL = "https://graph.facebook.com/v2.6/me/messages"
+
+
+class ActionProcessor(object):
+    action = None
+    request = None
+    sender_id = None
+
+    def __init__(self, action, request):
+        super(ActionProcessor, self).__init__()
+        self.action = action
+        self.request = request
+        self.__init_sender_id()
+
+    def __init_sender_id(self):
+        try:
+            self.sender_id = self.request.get("originalRequest").get("data").get("sender").get("id")
+        except AttributeError:
+            print("can't extract sender id.")
+
+    def process_request(self):
+        pass
+
+
+class BookingProcessor(ActionProcessor):
+    def process_request(self):
+        super(BookingProcessor, self).process_request()
+        try:
+            booking_date = self.request.get("result").get("parameters").get("date")
+            message = self.__get_message(booking_date)
+            self.__reserve_message(message)
+        except AttributeError:
+            print("can't extract booking date.")
+        return {}
+
+    def __get_message(self, booking_date):
+        return "{0} 병원 예약되어 있습니다.".format(booking_date)
+
+    def __get_message_reservation_time(self):
+        time = datetime.today()
+        time = time.replace(second=time.second + 30)
+        return time
+
+    def __reserve_message(self, message):
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(self.__send_message, 'date', run_date=self.__get_message_reservation_time(), args=message)
+        scheduler.start()
+
+    def __send_message(self, message):
+        params = {
+            "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        data = json.dumps({
+            "recipient": {
+                "id": self.sender_id
+            },
+            "message": {
+                "text": message
+            }
+        })
+
+        r = requests.post(FACEBOOK_SEND_URL, params=params, headers=headers, data=data)
+        if r.status_code != 200:
+            log(r.status_code)
+            log(r.text)
--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*- 
 # !/usr/bin/env python
 
-from datetime import datetime
 import json
 import os
 
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask import request
 from flask import make_response
-import requests
 
 # Flask app should start in global layout
+from action_processor import BookingProcessor
+
 APIAI_ACTION_NAME_BOOKING = "selvyCureBooking"
+
 app = Flask(__name__)
 
 FACEBOOK_SEND_URL = "https://graph.facebook.com/v2.6/me/messages"
@@ -34,55 +34,20 @@ def webhook():
 
 
 def process_request(req):
-    if req.get("result").get("action") != APIAI_ACTION_NAME_BOOKING:
+    response = None
+    try:
+        action = req.get("result").get("action")
+
+        if action == APIAI_ACTION_NAME_BOOKING:
+            response = BookingProcessor()
+    except AttributeError:
+        print("can't extract action.")
         return {}
 
-    try:
-        sender_id = req.get("originalRequest").get("data").get("sender").get("id")
-        booking_date = req.get("result").get("parameters").get("date")
-        message = get_message(booking_date)
-        reserve_message(sender_id, message)
-    except AttributeError:
-        print("can't extract sender id. ")
+    if response is not None:
+        return response.process_request()
+
     return {}
-
-
-def get_message(booking_date):
-    return "{0} 병원 예약되어 있습니다.".format(booking_date)
-
-
-def get_message_reservation_time():
-    time = datetime.today()
-    time = time.replace(second=time.second + 30)
-    return time
-
-
-def reserve_message(sender_id, message):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(send_message, 'date', run_date=get_message_reservation_time(), args=(sender_id, message))
-    scheduler.start()
-
-
-def send_message(recipient_id, message):
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message
-        }
-    })
-
-    r = requests.post(FACEBOOK_SEND_URL, params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
 
 
 if __name__ == '__main__':
